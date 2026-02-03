298ba908240861bebbc6359baf4eb9cea841306b
Agrawalarchi <archiagrawal387@gmail.com>
2026-02-03 23:10:06 +0530

    trying to solve cors error

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ const express = require('express');
 const app = express();
 const Router = require('./Routers/routes.js');
 const cors = require('cors');
+require('dotenv').config();
+
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());    
-app.use(cors({origin:"*", credentials:true}))
+app.use(cors({origin:process.env.FRONTEND_LINK_STRING, credentials:true}))
 app.listen(8080, ()=>{
     console.log("listening");
 
